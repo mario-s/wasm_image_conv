cd1eed0df40628f05f6acb1b63bccf6840a943ab
Mario-S <spindizzy@outlook.com>
2022-08-12 17:13:48 +0200

    test to read base 64 encoded image

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+extern crate image;
+extern crate base64;
+
+use base64::decode;
+use image::{DynamicImage, GenericImageView, ImageFormat};
+use image::imageops::grayscale;
 use wasm_bindgen::prelude::*;
 use web_sys::window;
 use wasm_bindgen::JsCast;
+
+const IMG: &str = "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=";
 
 #[wasm_bindgen]
 pub fn convert(name: &str) {
@@ -10,12 +18,23 @@ pub fn convert(name: &str) {
     let document = window.document().unwrap();
     let target = document.get_element_by_id("target").expect("document should have a target element");
 
-    let img = document.create_element("img")
+    let img = read_img(IMG);
+    let dim = img.dimensions();
+
+    let img_element = document.create_element("img")
     .unwrap()
     .dyn_into::<web_sys::HtmlImageElement>()
     .unwrap();
-    img.set_alt(&greetings);
-    img.set_name("output");
+    img_element.set_alt(&greetings);
+    img_element.set_name("output");
+    img_element.set_width(dim.0);
+    img_element.set_height(dim.1);
 
-    target.append_child(&img).unwrap();
+    target.append_child(&img_element).unwrap();
+}
+
+fn read_img(data: &str) -> DynamicImage {
+    let vec = decode(data).unwrap();
+    let buf = vec.as_slice();
+    return image::load_from_memory_with_format(buf, ImageFormat::Png).unwrap();
 }
